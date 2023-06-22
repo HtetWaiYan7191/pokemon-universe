3fb822201d64d42b7eca0f6d06a56bb74dc15e11
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-22 11:58:38 +0630

    Implement addReaction function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import './style.css';
 import getAllPokemons from './modules/getAllPokemons.js';
+import { create, get } from 'lodash';
 const pokemons_numbers = 10;
 const base_url = "https://pokeapi.co/api/v2/pokemon";
+const reaction_base_url = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi";
 const pokemon_cards_container = document.querySelector('.pokemon-cards-container')
 let pokemons = [];
+
+const getAppData = async () => {
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json; charset=UTF-8',
+        },
+      };
+        const result = await fetch(`${reaction_base_url}/apps/`, requestOptions);
+        const contentType = result.headers.get('content-type');
+        const data_text = await result.text();
+        let data;
+          data = data_text;
+         return data;
+};
+
+const getData = async () => {
+    const appId = await getAppData();
+    return appId;
+}
+
 
 const fetchPokemons = async () => {
     for(let i=1;i <= pokemons_numbers; i++) {
@@ -23,8 +46,8 @@ const createPokemonCard = (pokemon) => {
                     ${pokemon.name}
                 </figcaption>
                 <div class="reaction">
-                    <i class="fa-regular fa-heart"></i>
-                    <span>48</span>
+                    <i class="fa-regular fa-heart" id="${pokemon.id}"></i>
+                    <span class="reaction-count">48</span>
                 </div>
             </div>
             <div class="button-container">
@@ -34,7 +57,36 @@ const createPokemonCard = (pokemon) => {
                 <button class="m-2 w-100">Reservations</button>
             </div>   
         </div>
-    `
+    `;
+
+    const reaction_btns = document.querySelectorAll('.fa-heart');
+    reaction_btns.forEach((reaction_btn) => addReaction(reaction_btn, getData));
+    const reaction_count = document.querySelectorAll('reaction-count');
+
 }
 
+const addReaction = async (reaction_btn, id) => {
+    reaction_btn.addEventListener('click', async (e) => {
+        const appId = await id();
+        const item = {'item_id': `${e.target.id}`}
+        const url = `${reaction_base_url}/apps/${appId}/likes`;
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+              'content-type': 'application/json; charset=UTF-8',
+            },
+            body : JSON.stringify(item)
+          };
+
+          const result = await fetch(`${url}`,requestOptions);
+          const contentType = result.headers.get('content-type');
+          const resolve = await result.text();
+          console.log(resolve)
+    })
+   
+}
+
+const getReaction = async () => {
+
+}
 fetchPokemons();
