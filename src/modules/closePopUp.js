e29c823bb5c9d71f5aa93a8b3beb46f1b13aa4da
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-28 12:16:05 +0630

    Update the closePopUp function

--- src/modules/closePopUp.js
+++ src/modules/closePopUp.js
@@ -1,8 +1,10 @@
 import { popUpBox } from './getAppData';
+import { closePopupCard } from './popUp';
 
 const closePopUp = (closeCommentBtn, overLay) => {
   popUpBox.classList.remove('goBackPopUp');
   closeCommentBtn.addEventListener('click', () => {
+    closePopupCard();
     popUpBox.classList.remove('hidePopUp');
     popUpBox.classList.add('goBackPopUp');
     overLay.classList.add('hideOverlay');
